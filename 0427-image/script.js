4127ed010924ab7afe3466105ffabe4c7c8de139
Xiaozao Wang <xw2194@nyu.edu>
2023-05-17 23:56:34 +0800

    hhci

---
+++ 0427-image/script.js
@@ -0,0 +1,21 @@
+let img;
+
+
+
+function preload() {
+  img = loadImage("assets/colorful.jpg");
+}
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+
+
+  console.log(img.width);
+}
+
+function draw() {
+  image(img, 0, 0);
+}
+
+
